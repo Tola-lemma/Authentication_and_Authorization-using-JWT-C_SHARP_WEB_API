c0a806b9f22a60aff1d038daef97ac2d94afd940
Tola-lemma <tolalemma@gmail.com>
2023-02-25 01:00:42 -0800

    in AuthController method to register user is created with username and hashed password

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
 
 namespace JWTWebApi.Controllers
 {
@@ -7,5 +8,25 @@ namespace JWTWebApi.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        // create method to register user
+        public static User user = new User();
+        [HttpPost("Register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            CreatePasswordHash(request.Password,out byte[] passwordHash,out byte[] passwordSalt);
+            user.Username= request.Username;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+            return (user);
+        }
+        private void CreatePasswordHash(string password,out byte[] passwordHash,out byte[] passwordSalt)
+        {
+            // cryptograpy algorithm reference
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
     }
 }
