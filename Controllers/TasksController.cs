2f96fafc34d14b2a5134c866bac018a74c9782e7
Tola-lemma <tolalemma@gmail.com>
2023-02-25 14:44:41 -0800

    final commit

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,28 @@
+﻿using classTwoTodo.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace classTwoTodo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+  
+    public class TodosController : ControllerBase
+    {
+        public List<TodoItems> todos = new List<TodoItems>()
+        {
+        new TodoItems("001","Task one",false,"user001"),
+        new TodoItems("002","Task two",false,"user002"),
+        new TodoItems("003","Task three",true,"user003")
+        };
+        [HttpGet("Get Todos"),Authorize]
+        // [HttpGet("Get Todos"),Authorize(Roles ="Admin")]
+        public List<TodoItems> getAllTodos()
+        {
+            return todos;
+        }
+
+    }
+}
---
+++ Models/TodoItems.cs
@@ -0,0 +1,19 @@
+﻿namespace classTwoTodo.Models
+{
+    public class TodoItems
+    {
+        public string id { get; set; }
+        public string task { get; set; }
+        public bool completed { get; set; }
+        public string userID { get; set; }
+
+        public TodoItems(string id, string task, bool completed, string userID)
+        {
+            this.id = id;
+            this.task = task;
+            this.completed = completed;
+            this.userID = userID;
+        }
+    }
+
+}
