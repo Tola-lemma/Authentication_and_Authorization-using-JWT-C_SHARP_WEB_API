cfac196d9ceb9a9d289d6aae8596d0d904ae4104
Tola-lemma <tolalemma@gmail.com>
2023-02-25 02:04:55 -0800

    Password Verification is modified

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -44,7 +44,7 @@ namespace JWTWebApi.Controllers
         }
         private bool VerifyPaswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
         {
-            using(var hmac = new HMACSHA512(passwordHash))
+            using(var hmac = new HMACSHA512(passwordSalt))
             {
                 var computeHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
                 return computeHash.SequenceEqual(passwordHash);
